b47aa5f68ed1397cbe74a5891bfab35d377a3198
P.C.TEJA <pcteja2000@gmail.com>
2023-11-04 00:09:10 +0530

    second commit

---
+++ check2/check3/hello3.py
@@ -0,0 +1,7 @@
+def hello3():
+    """This code will print hello world"""
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    hello3()
---
+++ check2/hello2.py
@@ -0,0 +1,8 @@
+def hello2():
+    """This code will print hello world"""
+    print("Hello World")
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    hello2()
